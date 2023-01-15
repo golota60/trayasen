3680c2c70064bdac42621d55cad22fb65a403129
Szymon Wiszczuk <golota60@gmail.com>
2023-01-15 18:38:26 +0100

    feat: config path for linux

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+currently app needs an exact MAC address to work
+
 needed to work 
 https://github.com/tauri-apps/tao#linux
+
+
+dirs reference:
+https://tauri.app/v1/api/js/path#datadir
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,79 @@
-use futures::future;
-
 use idasen::get_instance_by_mac;
+use std::process::Command;
+use std::str;
 use tray_item::TrayItem;
+
+static CONFIG_FILE_NAME: &str = "config.json";
+static FOLDER_NAME: &str = "idasen-tray";
+static LINUX_DATA_DIR: &str = "$HOME/.local/share";
+
+/**
+ * TODO list:
+ * 1. store data somewhere in the system(system specific)
+ * 2. save & load stuff to that file(json probably)
+ */
+
+enum ConfigPaths {
+    Linux,
+    MacOS,
+    Windows,
+}
+
+impl ConfigPaths {
+    fn get_config_path(&self) -> String {
+        let path = format!(
+            "echo {}/{}/{}",
+            LINUX_DATA_DIR, FOLDER_NAME, CONFIG_FILE_NAME
+        );
+
+        match self {
+            ConfigPaths::Linux => {
+                let mut home = Command::new("sh")
+                    .arg("-c")
+                    .arg(path)
+                    .output()
+                    .expect("Failed to get $HOME path");
+
+                String::from_utf8(home.stdout).expect("Error while parsing $HOME path")
+            }
+            ConfigPaths::MacOS => todo!(),
+            ConfigPaths::Windows => todo!(),
+        }
+    }
+}
+
+fn load_config() -> String {
+    let sys = cfg!(windows);
+    let config = ConfigPaths::Linux.get_config_path();
+    config
+}
 
 // EE:4D:A2:34:E4:8F
 fn main() {
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
-    gtk::init().unwrap();
+    let rt = tokio::runtime::Runtime::new().expect("Error while initializing runtime");
+    gtk::init().expect("Error while initializing GTK");
 
-    let desk = rt.spawn(get_instance_by_mac("EE:4D:A2:34:E4:8F"));
-    let desk = rt.block_on(async { desk.await });
+    let path = load_config();
+    println!("{:?}", path);
+
+    // let desk = rt.spawn();
+    let desk = rt.block_on(async { get_instance_by_mac("EE:4D:A2:34:E4:8F").await });
     // TODO: Find a better way of unwrapping this shit
     let desk = match desk {
         Ok(desk) => desk,
         Err(error) => panic!("Error while connecting to the desk: {:?}", error),
     };
-    let desk = match desk {
-        Ok(desk) => desk,
-        Err(error) => panic!("Error while connecting to the desk2: {:?}", error),
-    };
 
     // TODO: how accessorries icons work?
     let mut tray = TrayItem::new("Idasen desk tray", "accessories-calculator").unwrap();
 
-    tray.add_label("Tray Label").unwrap();
+    tray.add_label("Idasen controller").unwrap();
 
-    tray.add_menu_item("Hello", move || {
-        // TODO: this doesn't execute - tokio should be able to fix this
+    tray.add_menu_item("Bring up", move || {
         rt.block_on(async {
             println!("Trying to bring your idasen up!");
-            desk.move_to(8000).await;
+            desk.move_to(8000).await.unwrap();
         });
-
-        // .await?;
     })
     .unwrap();
 
@@ -40,7 +81,6 @@ fn main() {
         gtk::main_quit();
     })
     .unwrap();
-    // test_func().await;
 
     gtk::main();
 }

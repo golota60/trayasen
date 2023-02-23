77f7eb393fa04404174d335b3d9ce6b83bf9a27c
Steam Deck User <golota60@gmail.com>
2023-02-23 20:33:06 +0100

    use tailwind for animation

--- src/generic/Spinner.tsx
+++ src/generic/Spinner.tsx
@@ -2,8 +2,19 @@ interface Props {
     size: 'sm' | 'md' | 'lg';
 }
 
+const getSize = (size: Props['size']) => {
+    switch (size) {
+        case "sm":
+            return 'w-6';
+        case "md":
+            return 'w-10';
+        case "lg":
+            return 'w-14';
+    }
+}
+
 const Spinner = ({ size = 'sm' }: Props) => {
-    return <img className={`spinner spinner-${size}`} src="/carrot.png" alt="A carrot logo" />;
+    return <img className={`animate-[spin_1s_linear_infinite] ${getSize(size)}`} src="/carrot.png" alt="A carrot logo" />;
 }
 
 export default Spinner;

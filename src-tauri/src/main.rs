e170378ae98fa1c5571f95ccbcf7e00f87e75ced
Szymon Wiszczuk <golota60@gmail.com>
2023-03-03 20:55:33 +0100

    feat: way better hidden window handling

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -135,20 +135,22 @@ fn main() {
         .setup(move |app| {
             let loc_name = &config.local_name;
 
+            let win = app
+                .get_window("main")
+                .expect("Error while getting main window window on init");
             match loc_name {
                 // If saved name is defined, don't open the initial window
                 Some(e) => {
                     println!("config found. closing main window");
 
-                    let win = app
-                        .get_window("main")
-                        .expect("Error while getting main window window on init");
                     win.close().expect("Error while closing the window");
                     block_on(async {
                         connect_to_desk_by_name(e.to_string(), app.state::<SharedDesk>()).await;
                     });
                 }
-                None => {}
+                None => {
+                    win.show();
+                }
             }
             Ok(())
         })

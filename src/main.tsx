e22823c881c288cf8fec08722ed44c1cf616ea8c
Szymon Wiszczuk <golota60@gmail.com>
2023-12-25 20:17:26 +0100

    use iife for backward compat

--- src/main.tsx
+++ src/main.tsx
@@ -5,22 +5,24 @@ import App from "./App";
 import "./style.css";
 import { hasCustomDecorations } from "./rustUtils";
 
-const customDecorations = await hasCustomDecorations();
+(async () => {
+  const customDecorations = await hasCustomDecorations();
 
-if (customDecorations) {
-  document
-    ?.getElementById("titlebar-minimize")
-    ?.addEventListener("click", () => appWindow.minimize());
-  document
-    ?.getElementById("titlebar-maximize")
-    ?.addEventListener("click", () => appWindow.toggleMaximize());
-  document
-    ?.getElementById("titlebar-close")
-    ?.addEventListener("click", () => appWindow.close());
-} else {
-  // if window doesn't have custom decorations, remove the titlebar altogether
-  document?.querySelector(".titlebar")?.remove();
-}
+  if (customDecorations) {
+    document
+      ?.getElementById("titlebar-minimize")
+      ?.addEventListener("click", () => appWindow.minimize());
+    document
+      ?.getElementById("titlebar-maximize")
+      ?.addEventListener("click", () => appWindow.toggleMaximize());
+    document
+      ?.getElementById("titlebar-close")
+      ?.addEventListener("click", () => appWindow.close());
+  } else {
+    // if window doesn't have custom decorations, remove the titlebar altogether
+    document?.querySelector(".titlebar")?.remove();
+  }
+})();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
